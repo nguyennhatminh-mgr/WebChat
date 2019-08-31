95efe905a8ce1b93e6245db260039984bc8cbf7c
Nguyễn Đăng Huy <huy.nguyen.gl99@hcmut.edu.vn>
2019-08-31 23:37:26 +0700

    Huy done some of firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBOoFgp-EA3dkz0lDgubSPuYRw-QQnRnxo",
+    authDomain: "angularfire-1aa89.firebaseapp.com",
+    databaseURL: "https://angularfire-1aa89.firebaseio.com",
+    projectId: "angularfire-1aa89",
+    storageBucket: "",
+    messagingSenderId: "330366994048",
+    appId: "1:330366994048:web:9bf968745366b25f"
+  }
 };
 
 /*
